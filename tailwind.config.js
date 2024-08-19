4376fdb79142faf507bc0524eda488c37def1ab7
Ali-dawud2023 <alidawoud203@gmail.com>
2024-08-19 12:54:10 +0300

    install tailwindcss and bladewind and TW elemenets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/tw-elements/js/**/*.js"
     ],
 
     theme: {
@@ -16,6 +20,6 @@ export default {
             },
         },
     },
-
-    plugins: [forms],
+    darkMode: "class",
+    plugins: [require("tw-elements/plugin.cjs")],
 };
